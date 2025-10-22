d211f14f2cead9768d9df142e012c6a7ebe37d24
Julien Blanchon <julien@blanchon.cc>
2025-10-22 16:54:03 +0100

    Fix silocu

--- Casks/SiloCU.rb
+++ Casks/SiloCU.rb
@@ -1,11 +1,11 @@
-cask "SiloCU" do
+cask "silocu" do
   version "1.2.2"
   sha256 "aeff7e23a3f0020557a70fbb7f1edf0198828dc9b4ff81c772ec5b10223bdcda"
 
   url "https://github.com/julien-blanchon/silo/releases/download/v1.2.2/silo_aarch64.app.tar.gz",
       verified: "github.com/julien-blanchon/silo/"
 
-  name "SiloCU"
+  name "silocu"
   desc "AI-powered desktop app built with Tauri"
   homepage "https://github.com/julien-blanchon/silo"
 
